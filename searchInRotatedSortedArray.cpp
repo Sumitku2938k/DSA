b487f4a0210e92bc0bf36f901590fac8c4a2cdf6
Sumit Kumar <sumitku2938k@gmail.com>
2026-03-27 13:42:15 +0530

    Search in Rotated Sorted Array implementation (Optimized  way)

--- searchInRotatedSortedArray.cpp
+++ searchInRotatedSortedArray.cpp
@@ -3,28 +3,35 @@ using namespace std;
 
 int search(int *nums, int n, int target){
     int low = 0, high = n-1;
-    int mid = low + (high-low)/2;
+    
+    while (low<=high)
+    {
+        int mid = low + (high - low) / 2;
 
-    if(nums[low] <= nums[mid]) {
-        for(int i=low; i<=mid; i++){
-            if(nums[i]==target){
-                return i;
-            }
+        if (nums[mid] == target)
+        {
+            return mid;
         }
-    }else{
-        for(int i=mid; i<=high; i++){
-            if(nums[i]==target){
-                return i;
-            }
+        
+
+        if (nums[low] <= nums[mid]){
+            if (nums[low] <= target && target < nums[mid])
+                high = mid-1;
+            else
+                low = mid+1;
+        }else{
+            if (nums[mid] < target && target <= nums[high])
+                low = mid+1;    
+            else
+                high = mid-1;
         }
     }
     return -1;
-    
 }
 
 int main(){
     int nums[7] = {4,5,6,7,0,1,2};
-    int target = 7;
+    int target = 2;
     int n = sizeof(nums)/sizeof(int);
 
     cout << search(nums, n, target) << endl;
