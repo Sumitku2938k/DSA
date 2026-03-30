574d9e23d1fcbde3f9e73f94a961a1fd9f0aac04
Sumit Kumar <sumitku2938k@gmail.com>
2026-03-30 21:10:16 +0530

    Refactor binarySort function to improve efficiency with early exit condition where the array is already sorted

--- binarySort.cpp
+++ binarySort.cpp
@@ -4,12 +4,18 @@ using namespace std;
 void binarySort(int *arr, int n){
     for (int i = 0; i < n-1; i++)
     {
+        bool isSwap = false;
         for (int j = 0; j < n-i-1; j++)
         {
             if (arr[j] > arr[j+1])
             {
                 swap(arr[j], arr[j+1]);
+                isSwap = true;
             }
+        }
+        if (!isSwap)
+        {
+            break;
         }
     }
 
@@ -21,7 +27,7 @@ void binarySort(int *arr, int n){
 }
 
 int main(){
-    int arr[] = {4,1,5,2,3};
+    int arr[] = {1,2,3,4,5,6,7,8,9,10};
     int n = sizeof(arr)/sizeof(int);
 
     binarySort(arr, n);
