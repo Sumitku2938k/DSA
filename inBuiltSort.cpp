0df48b6ccd01fbe6e5dbd75e9727f6014661b3c9
Sumit Kumar <sumitku2938k@gmail.com>
2026-04-06 21:23:15 +0530

    In built Descending order sorting

--- inBuiltSort.cpp
+++ inBuiltSort.cpp
@@ -6,7 +6,12 @@ int main(){
     int arr[] = {1,4,1,3,2,4,3,7};
     int n = sizeof(arr)/sizeof(int);
 
-    sort(arr, arr+n);
+    // Inbuilt sort function (Ascending order)
+    // sort(arr, arr+n);
+
+    // Inbuilt sort function (Descending order)
+    sort(arr, arr+n, greater<int>());
+
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
