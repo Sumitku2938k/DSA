5275e8289866fbca6625cc5a25dab283ad9459ac
Sumit Kumar <sumitku2938k@gmail.com>
2026-04-06 21:15:12 +0530

    In built Ascending Order Sorting in c++

---
+++ inBuiltSort.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int arr[] = {1,4,1,3,2,4,3,7};
+    int n = sizeof(arr)/sizeof(int);
+
+    sort(arr, arr+n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
