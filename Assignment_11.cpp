7b2f28f4299c199f472289dcd693370da9259ea4
Sumit Kumar <sumitku2938k@gmail.com>
2026-04-07 13:38:45 +0530

    Assignment 11: Implement binary sort algorithm in descending order in C++.

---
+++ Assignment_11.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+void printArray(int arr[], int n){
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+void binarySort(int *arr, int n){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n-i-1; j++)
+        {
+            if (arr[j] < arr[j+1])
+            {
+                swap(arr[j], arr[j+1]);
+            }
+            
+        }
+    }
+    printArray(arr, n);   
+}
+
+int main(){
+    int arr[] = {3, 6, 2, 1, 8, 7, 4, 5, 3, 1};
+    int n = sizeof(arr)/sizeof(int);
+
+    binarySort(arr, n);
+
+    return 0;
+}
