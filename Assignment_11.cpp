868f73bec1abe3a363e3de341e03e06cbbf7fa56
Sumit Kumar <sumitku2938k@gmail.com>
2026-04-07 13:54:45 +0530

    Assignment 11: Selection sort algorithm in descending order in C++.

--- Assignment_11.cpp
+++ Assignment_11.cpp
@@ -8,26 +8,45 @@ void printArray(int arr[], int n){
     }
 }
 
-void binarySort(int *arr, int n){
-    for (int i = 0; i < n; i++)
+// void binarySort(int *arr, int n){
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < n-i-1; j++)
+//         {
+//             if (arr[j] < arr[j+1])
+//             {
+//                 swap(arr[j], arr[j+1]);
+//             }
+            
+//         }
+//     }
+//     printArray(arr, n);   
+// }
+
+void selectionSort(int *arr, int n){
+    cout << "Selection Sort: " << endl;
+    for (int i = 0; i < n-1; i++)
     {
-        for (int j = 0; j < n-i-1; j++)
+        int maxIdx = i;
+        for (int j = i+1; j < n; j++)
         {
-            if (arr[j] < arr[j+1])
+            if (arr[j] > arr[maxIdx])
             {
-                swap(arr[j], arr[j+1]);
+                maxIdx = j;
             }
             
         }
+        swap(arr[i], arr[maxIdx]);
     }
-    printArray(arr, n);   
+    printArray(arr, n);
 }
 
 int main(){
     int arr[] = {3, 6, 2, 1, 8, 7, 4, 5, 3, 1};
     int n = sizeof(arr)/sizeof(int);
 
-    binarySort(arr, n);
+    // binarySort(arr, n);
+    selectionSort(arr, n);
 
     return 0;
 }
