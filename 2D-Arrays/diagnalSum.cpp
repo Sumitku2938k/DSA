7c25499b78f93f69b688545bc00b674316490c32
Sumit Kumar <sumitku2938k@gmail.com>
2026-04-23 21:08:56 +0530

    Diagonal Sum Optimized to O(n) from O(n^2)

--- 2D-Arrays/diagnalSum.cpp
+++ 2D-Arrays/diagnalSum.cpp
@@ -4,20 +4,29 @@ using namespace std;
 void diagnalSum(int mat[][4], int n){
     int sum = 0;
 
-    for (int i = 0; i < n; i++) // O(n^2)
+    // for (int i = 0; i < n; i++) // O(n^2)
+    // {
+    //     for (int j = 0; j < n; j++)
+    //     {
+    //         if (i == j) //pd
+    //         {
+    //             sum += mat[i][j];
+    //         }else if (j == n-i-1) //sd
+    //         {
+    //             sum += mat[i][j];
+    //         }
+    //     }
+    // }
+
+    for (int i = 0; i < n; i++) //O(n)
     {
-        for (int j = 0; j < n; j++)
+        sum += mat[i][i]; //pd
+        if (i != n-i-1)
         {
-            if (i == j) //pd
-            {
-                sum += mat[i][j];
-            }else if (j == n-i-1) //sd
-            {
-                sum += mat[i][j];
-            }
+            sum += mat[i][n-i-1]; //sd
         }
     }
-
+    
     cout<<"Sum = "<<sum<<endl;
     
 }
