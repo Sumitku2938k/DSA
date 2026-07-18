078d1e5d5861e3405c58abdddedf8a878e574bb1
Sumit Kumar <sumitku2938k@gmail.com>
2026-07-18 13:30:09 +0530

    Container With Most Water Problem [LeetCode 11] implemented and added to Assignment_14.cpp

--- Vectors/Assignment_14.cpp
+++ Vectors/Assignment_14.cpp
@@ -18,10 +18,30 @@ vector<int> setMismatch(vector<int> nums){
     return ans;
 }
 
-int main(){
-    vector<int> nums = {1,2,2,4};
+int containerWithMostWater(vector<int> height){
+    int currArea = 0, maxArea = 0;
 
-    vector<int> ans = setMismatch(nums);
-    cout<<"duplicate= "<<ans[0]<<", missing= "<<ans[1];
+    for (int i = 0; i < height.size(); i++)
+    {
+        for (int j = i+1; j < height.size(); j++)
+        {
+            int width = j - i;
+            int length  =  min(height[i], height[j]);
+
+            currArea = length * width;
+            maxArea = max(maxArea, currArea);
+        }
+    }
+    return maxArea;
+}
+
+int main(){
+    // vector<int> nums = {1,2,2,4};
+
+    // vector<int> ans = setMismatch(nums);
+    // cout<<"duplicate= "<<ans[0]<<", missing= "<<ans[1];
+    
+    vector<int> height = {1,8,6,2,5,4,8,3,7};
+    cout << "Max Area = " << containerWithMostWater(height) << endl;
     return 0;
 }
